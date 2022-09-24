8c4f4f94354e62a16a8b4b5a48e97fc9c44e08a5
nikzan4 <76505715+nikzan4@users.noreply.github.com>
2022-09-24 20:34:44 -0300

    Add files via upload

---
+++ HilosActividad3/Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 33)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(527, 24)
+        Me.ProgressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous
+        Me.ProgressBar1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(49, 72)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "INICIAR"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(464, 72)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "CANCELAR"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(254, 72)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(94, 31)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(547, 33)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 24)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "0%"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 128)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(618, 225)
+        Me.TextBox2.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(640, 372)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ HilosActividad3/Form1.vb
@@ -0,0 +1,61 @@
+﻿Imports System.IO
+Imports System.Threading
+Public Class Form1
+    Dim hiloEscrituraArchivo As Thread
+
+    Private synchronizationContext As SynchronizationContext
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        synchronizationContext = SynchronizationContext.Current
+    End Sub
+
+    Private Sub escribirArchivo()
+        File.Create("archivo.txt").Dispose()
+        Dim sw As New StreamWriter("archivo.txt")
+        Try
+            Dim cantidad As UInteger = Integer.Parse(TextBox1.Text)
+            Dim incremento As Double = 100 / cantidad
+            Dim valor As Double = 0
+            For index = 1 To Integer.Parse(TextBox1.Text)
+                sw.WriteLine("Linea: {0}.", index)
+                valor += incremento
+                synchronizationContext.Send(New SendOrPostCallback(Sub()
+                                                                       Label1.Text = ProgressBar1.Value & "%"
+                                                                       ProgressBar1.Value = valor
+                                                                       Label1.Refresh()
+                                                                   End Sub), Nothing)
+            Next
+            MsgBox("Terminado!")
+        Catch ex As Exception
+            If TypeOf ex Is ThreadAbortException Then
+                sw.Dispose()
+                File.Delete("archivo.txt")
+                MsgBox("Cancelado con exito!")
+            Else
+                MsgBox(ex.Message, MsgBoxStyle.Critical, "Error al escribir archivo")
+            End If
+        Finally
+            sw.Dispose()
+            sw.Close()
+            synchronizationContext.Send(New SendOrPostCallback(Sub()
+                                                                   Label1.Text = 0 & "%"
+                                                                   ProgressBar1.Value = 0
+                                                                   Label1.Refresh()
+                                                               End Sub), Nothing)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        hiloEscrituraArchivo = New Thread(AddressOf escribirArchivo) With {.IsBackground = True}
+        hiloEscrituraArchivo.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If hiloEscrituraArchivo.IsAlive Then
+            hiloEscrituraArchivo.Abort()
+        Else
+            MsgBox("No hay nada que cancelar.")
+        End If
+    End Sub
+
+End Class
