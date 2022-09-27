dd77793a12dd706504cd54fd093ab087ed651f43
nikzan4 <76505715+nikzan4@users.noreply.github.com>
2022-09-27 09:20:29 -0300

    Add files via upload

---
+++ BackgroundWorker/Form1.vb
@@ -0,0 +1,22 @@
+﻿Imports System.ComponentModel
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        BackgroundWorker1.RunWorkerAsync()
+    End Sub
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        Dim i As Integer
+        For i = 1 To 1000
+            BackgroundWorker1.ReportProgress(i / 10)
+            Threading.Thread.Sleep(500)
+        Next
+    End Sub
+
+    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        ProgressBar1.Value = e.ProgressPercentage
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        MsgBox("listo")
+    End Sub
+End Class
